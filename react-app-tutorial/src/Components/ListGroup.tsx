88e10cf6b5aaf35694f05c6bab43945c586596ca
Shirahn <shirahn@gendac.co.za>
2024-05-31 13:32:49 +0200

    Better way of displaying message

--- react-app-tutorial/src/Components/ListGroup.tsx
+++ react-app-tutorial/src/Components/ListGroup.tsx
@@ -1,10 +1,14 @@
 function ListGroup() {
   let items = ["Hello", "Goodbye", "South", "Africa"];
 
+  const getMessage = () => {
+    return items.length === 0 && <p> Empty List</p>;
+  };
+
   return (
     <>
       <h1>New list of items</h1>
-      {items.length === 0 ? <p>Empty List</p> : null}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
